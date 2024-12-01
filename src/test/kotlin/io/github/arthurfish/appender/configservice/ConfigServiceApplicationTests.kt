ef8f4e19fd7305631f7304c0a0ddf6db9615cd53
Arthur <arthurfish8051@gmail.com>
2024-12-01 15:00:27 +0800

    first commit

---
+++ src/test/kotlin/io/github/arthurfish/appender/configservice/ConfigServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.arthurfish.appender.configservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
